c39dbc8c1b53ad964fa198fb03ec77b526a95848
sanglq3 <sanglq3@vng.com.vn>
2023-02-23 23:16:25 +0700

    Add cors option for app

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
+
+  const corsOptions: cors.CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  };
+
+  // Enable CORS middleware with options
+  app.use(cors(corsOptions));
+
   await app.listen(port, () => {
     console.log('Server is running on port: ', port);
   });
